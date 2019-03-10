44e441f672c77fb160b9619e9f23e4515e695b47
RyanLinrm <runminlin@gmail.com>
2019-03-10 18:54:05 -0400

    import and export added

--- src/AddBooks/addBooks.js
+++ src/AddBooks/addBooks.js
@@ -1,6 +1,11 @@
 import React from 'react';
-
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const addBooks = (props) =>{
-    
+
 }
+
+
+export default addBooks;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import './App.css';
+import addBooks from './AddBooks/addBooks.js';
 
 class App extends Component {
+  state = {
+    "addBooks": [
+      { 
+          FirstName: "Cathy" ,
+          LastName: "Pierce",
+          Birthday: "9/14/1996",
+          Telephone: "200-910-8132"
+      },
+      {
+          FirstName: "Alfonso",
+          LastName: "Cooley",
+          Birthday: "8/10/1973",
+          Telephone: "200-657-9362"
+      },
+      {
+          FirstName: "Victor",
+          LastName: "Gordon",
+          Birthday:  "8/3/1970",
+          Telephone: "200-661-9407"
+      },
+      {
+          FirstName: "Colleen",
+          LastName: "Wright",
+          Birthday: "10/28/1967",
+          Telephone: "200-250-7949"
+      },
+      {
+          FirstName: "James",
+          LastName: "Johnston",
+          Birthday: "5/11/1972",
+          Telephone: "200-645-3176"
+      },
+      {
+          FirstName: "Anna",
+          LastName: "Reyes",
+          Birthday: "9/10/1975",
+          Telephone: "200-707-8670"
+      }
+  ]
+  }
+  
+  
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
       </div>
     );
   }
