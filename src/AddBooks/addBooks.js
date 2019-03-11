13da4e4abf8ea3eb520b8c6eef54ab24dbcbe5cd
RyanLinrm <runminlin@gmail.com>
2019-03-10 21:59:00 -0400

    Display all the state content

--- src/AddBooks/addBooks.js
+++ src/AddBooks/addBooks.js
@@ -4,8 +4,20 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 const addBooks = (props) =>{
-
-}
+    let addBook = [...props.books];
+    let output = addBook.map( book =>
+        <div>
+            <div class="list-group0">
+                <Card class="border border-dark">
+                        <h4>{book.FirstName}{book.LastName}</h4>
+                        <h5>{book.Birthday}</h5>
+                        <p>{book.Telephone}</p>
+                </Card>
+            </div>
+        </div>
+    );
+    return output;
+};
 
 
 export default addBooks;
--- src/App.js
+++ src/App.js
@@ -2,42 +2,48 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import './App.css';
-import addBooks from './AddBooks/addBooks.js';
+import AddBooks from './AddBooks/addBooks.js';
 
 class App extends Component {
   state = {
     "addBooks": [
       { 
+          key: 'k1',
           FirstName: "Cathy" ,
           LastName: "Pierce",
           Birthday: "9/14/1996",
           Telephone: "200-910-8132"
       },
       {
+          key: 'k2',
           FirstName: "Alfonso",
           LastName: "Cooley",
           Birthday: "8/10/1973",
           Telephone: "200-657-9362"
       },
       {
+          key: 'k3',
           FirstName: "Victor",
           LastName: "Gordon",
           Birthday:  "8/3/1970",
           Telephone: "200-661-9407"
       },
       {
+          key: 'k4',
           FirstName: "Colleen",
           LastName: "Wright",
           Birthday: "10/28/1967",
           Telephone: "200-250-7949"
       },
       {
+          key: 'k5',
           FirstName: "James",
           LastName: "Johnston",
           Birthday: "5/11/1972",
           Telephone: "200-645-3176"
       },
       {
+          key: 'k6',
           FirstName: "Anna",
           LastName: "Reyes",
           Birthday: "9/10/1975",
@@ -51,7 +57,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Container>
+          <h1>Address-Book-app</h1>
+          <AddBooks books={this.state.addBooks}/>
+        </Container>
       </div>
     );
   }
